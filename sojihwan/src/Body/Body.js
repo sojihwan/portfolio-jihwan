6cf69513f8c5516e30fe21133ba573db11f53fdd
sojihwan <73238543+sojihwan@users.noreply.github.com>
2022-02-24 12:59:12 +0900

    2/24

--- sojihwan/src/Body/Body.js
+++ sojihwan/src/Body/Body.js
@@ -2,11 +2,6 @@ import React from "react";
 import '../Body/Body.css'
 
 function Body(){
-  const Clicked = () =>{
-      var beg = document.getElementsByClassName("png");
-      beg.style.
-
-  }
   return(
     <div className="body-table">
       <table border='1' className='type10'>
@@ -23,12 +18,13 @@ function Body(){
               <td scope="row">Youlist</td>
               <td scope="row">2021-10-18~ing</td>
               <td scope="row">자작 유튜브 모음 사이트</td>
+              <td><a><button>이동</button></a></td>
             </tr>
             <tr className="even">
               <td scope="row">rush hour</td>
               <td scope="row">2021-09-18~ing</td>
               <td scope="row">러시 아워 게임 사이트</td>
-              <td><img src="Img/rush_hour_now.png" className="png" onClick={Clicked}/></td>
+              <td><a><button>이동</button></a></td>
             </tr>
             <tr className="even">
               <td scope="row">사진</td>
@@ -39,7 +35,7 @@ function Body(){
             <tr className="even">
               <td scope="row">my 유튜브</td>
               <td scope="row">R.I.P</td>
-              <td scope="row">유튜브 동영상(현재 유튜브 자체 편집기능이 사라져서 동영상 2개(초3떄))</td>
+              <td scope="row">유튜브 동영상(현재 유튜브 자체 업로드가 사라져서 동영상 2개(초3떄))</td>
               <td><a href="https://www.youtube.com/channel/UC4yhSBygUqX0VFr3W6FTokw"><button>이동</button></a></td>
             </tr>
         </tbody>
